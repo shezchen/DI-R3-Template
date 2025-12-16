14156409e2d509d9c36bc3c2c82b66559d8b9925
熬夜拧螺丝 <shengzechen23@mails.ucas.ac.cn>
2025-12-16 22:44:30 +0800

    init

---
+++ Assets/Scripts/Gameplay/Debug/MouseSelect.cs
@@ -0,0 +1,37 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+namespace Gameplay
+{
+    public class MouseSelect : MonoBehaviour
+    {
+        [SerializeField] private Tilemap targetTilemap;
+
+        [ShowInInspector]public Vector3Int CurrentCellPos { get; private set; }
+
+        private void Update()
+        {
+            if (Camera.main != null)
+            {
+                Vector2 mousePos = Mouse.current.position.ReadValue();
+                Ray ray = Camera.main.ScreenPointToRay(mousePos);
+                if (targetTilemap != null)
+                {
+                    Plane plane = new Plane(Vector3.forward, Vector3.zero);
+                    if (plane.Raycast(ray, out float enter))
+                    {
+                        Vector3 worldPoint = ray.GetPoint(enter);
+                        Vector3Int cellPos = targetTilemap.WorldToCell(worldPoint);
+
+                        if (targetTilemap.HasTile(cellPos))
+                        {
+                            CurrentCellPos = cellPos;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Debug/TestTMPController.cs
@@ -0,0 +1,58 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class TestTMPController : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro q;
+        [SerializeField] private TextMeshPro s;
+        [SerializeField] private TextMeshPro r;
+        [SerializeField] private TextMeshPro center;
+        [SerializeField] private TextMeshPro rawOffset;
+        private CubeCoordinate _selfCoordinate;
+        private Vector2Int _selfOffset;
+
+        private void Awake()
+        {
+            SetAll(false);
+        }
+
+        public void SetSelfCoordinate(CubeCoordinate coordinate,Vector2Int offset)
+        {
+            _selfCoordinate = coordinate;
+            _selfOffset = offset;
+        }
+
+        public void SetQSR(bool active)
+        {
+            q.gameObject.SetActive(active);
+            s.gameObject.SetActive(active);
+            r.gameObject.SetActive(active);
+            q.text = _selfCoordinate.Q > 0 ? "+" + _selfCoordinate.Q.ToString() : _selfCoordinate.Q.ToString();
+            s.text = _selfCoordinate.S > 0 ? "+" + _selfCoordinate.S.ToString() : _selfCoordinate.S.ToString();
+            r.text = _selfCoordinate.R > 0 ? "+" + _selfCoordinate.R.ToString() : _selfCoordinate.R.ToString();
+        }
+
+        public void SetCube(bool active)
+        {
+            center.gameObject.SetActive(active);
+            center.text = _selfCoordinate.ToString();
+        }
+
+        public void SetOffset(bool active)
+        {
+            rawOffset.gameObject.SetActive(active);
+            rawOffset.text = _selfOffset.ToString();
+        }
+
+        public void SetAll(bool active)
+        {
+            q.gameObject.SetActive(active);
+            s.gameObject.SetActive(active);
+            r.gameObject.SetActive(active);
+            center.gameObject.SetActive(active);
+            rawOffset.gameObject.SetActive(active);
+        }
+    }
+}
